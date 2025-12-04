d2cd4ecdc1a9ecf207c55f843c13ebd286dc92b5
Kyaw San Oo <skyaw6736@gmail.com>
2025-12-04 12:14:17 +0630

    fix some error

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ClerkAuthGuard } from './auth/guards/clerk-auth.guard';
-import { clerkClient } from '@clerk/clerk-sdk-node';
+
 import { RoleGuard } from './auth/guards/role.guard';
 import { Role, ROLES } from './auth/decorators/role.decorator';
 import { Response } from 'express';
@@ -10,8 +10,11 @@ import { Response } from 'express';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(ClerkAuthGuard, RoleGuard)
+  @ROLES(Role.ADMIN)
   @Get()
-  getHello() {
-    return 'Hello! Strawberry';
+  async cats(@Req() req: any, @Res() res: any) {
+    const userId = req.user.id;
+    return this.appService.getUser(userId, res);
   }
 }
